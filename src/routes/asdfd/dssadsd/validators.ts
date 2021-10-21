102a3d3669c668ff83e12fbcd57aa05628efd9a6
chen <sbchens88@gmail.com>
2021-10-21 21:29:31 +0000

    Generated API: GET  asdfd/dssadsd/sdssd1

--- src/routes/asdfd/dssadsd/validators.ts
+++ src/routes/asdfd/dssadsd/validators.ts
@@ -1,4 +1,6 @@
 import { InputCheckChain } from 'src/types';
 export const Ojihugyvh: InputCheckChain[] = []
 export const Ojihugyvh: InputCheckChain[] = []
+export const Ojihugyvh: InputCheckChain[] = []
+
 
