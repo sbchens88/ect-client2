8c0b0b59b5b1df17c9640f8974db1b9f281b65ee
chen <sbchens88@gmail.com>
2021-11-21 16:46:31 +0000

    Generated API: POST  /api/chentest11-16/testing13

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import mountRPG from './rpg';
 import mountSQL from './sql';
+import mountCHENTEST11_16 from './chentest11_16';
 
 export default function mountAPI(router: Router) {
     // You can set auth requirements on a whole API section by putting `router.use(requireAuth);` here instead of on individual route definitions
@@ -12,4 +13,8 @@ export default function mountAPI(router: Router) {
     const sql = express.Router();
     mountSQL(sql);
     router.use('/sql', sql);
+
+    const chentest11_16 = Router();
+    mountCHENTEST11_16(chentest11_16);
+    router.use('/chentest11-16', chentest11_16);
 }
