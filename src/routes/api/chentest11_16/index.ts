725531837b799d92a9acdd11599d8a708fef5ad4
chen <sbchens88@gmail.com>
2021-11-22 10:59:12 +0000

    Generated API: GET  /api/chentest11-16/testing13

---
+++ src/routes/api/chentest11_16/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as chentest1116Controller from 'src/controllers/chentest11_16';
+import validate from 'src/middlewares/validate';
+import respond from 'src/middlewares/respond';
+import * as validators from './validators';
+import * as chentest1116Controller from 'src/controllers/chentest_1116';
+
+export default function mountCHENTEST11_16(router: Router) {
+    router.get(
+        '/testing13',
+        validate(validators.Safdgbvc),
+        respond((req: any) => chentest1116Controller.Safdgbvc(Object.assign({}, req.params, req.body, req.query)))
+    );
+}
